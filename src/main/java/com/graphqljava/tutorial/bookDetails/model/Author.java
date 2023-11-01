5037dfdbdf0e7503a93de8dce0a7edeed525f7ad
rduval <duvalromainn@gmail.com>
2023-11-01 16:57:48 +0100

    quality

--- src/main/java/com/graphqljava/tutorial/bookDetails/model/Author.java
+++ src/main/java/com/graphqljava/tutorial/bookDetails/model/Author.java
@@ -16,35 +16,6 @@ public class Author {
     public Author() {
     }
 
-    public Author(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
     @Override
     public String toString() {
         return "Author{" +
