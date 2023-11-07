90aad620298ae3a88a58180e79cc3b9cda6971f7
rduval <duvalromainn@gmail.com>
2023-11-07 22:43:24 +0100

    update

--- src/main/java/com/graphqljava/tutorial/bookDetails/controller/BookController.java
+++ src/main/java/com/graphqljava/tutorial/bookDetails/controller/BookController.java
@@ -34,16 +34,6 @@ public class BookController {
         return bookService.getBooks();
     }
 
-//    @QueryMapping
-//    public List<Book> books(@Argument List<Integer> ids, @Argument List<Integer> authorIds) {
-//        return bookService.getBooks(ids, authorIds);
-//    }
-
-//    @SchemaMapping
-//    public Author author(Book book) {
-//        return authorService.getAuthor(book.getAuthorId());
-//    }
-
     @BatchMapping
     public Mono<Map<Book, Author>> author (List<Book> books) {
         return Mono.fromSupplier(() -> {
